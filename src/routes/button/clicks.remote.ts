f2f15888f6528a091ce7fd5b6e6a7aabea04d219
Rich Harris <rich.harris@vercel.com>
2025-07-20 17:40:45 -0400

    fix

--- src/routes/button/clicks.remote.ts
+++ src/routes/button/clicks.remote.ts
@@ -2,10 +2,9 @@ import { command, query } from '$app/server';
 
 let clicks = 0;
 
-const sleep = (ms = 500) => new Promise((resolve) => setTimeout(resolve, ms));
+const sleep = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const getClicks = query(async () => {
-	await sleep();
 	return clicks;
 });
 
